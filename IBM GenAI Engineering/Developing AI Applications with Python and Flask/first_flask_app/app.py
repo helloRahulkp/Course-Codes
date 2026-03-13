84a37718ea42a9bfd261ba1b4b749ac86c61c20d
helloRahulkp <hello.rahulkp@gmail.com>
2026-03-13 10:59:31 +0530

    first flask app

--- IBM GenAI Engineering/Developing AI Applications with Python and Flask/first_flask_app/app.py
+++ IBM GenAI Engineering/Developing AI Applications with Python and Flask/first_flask_app/app.py
@@ -7,4 +7,16 @@ app = Flask(__name__)
 
 def hello_world():
     return "<b> My First Application in action ! <br>"
- # routing
+# routing
+
+# to run flask application 
+# open terminal in flask app dir
+# activate venv
+# export flask settings 
+
+'''export FLASK_APP=app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1'''
+
+# run flask app
+# flask run then go to localhost
