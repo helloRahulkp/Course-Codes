f3097970eb0b459edb0eeb1a11271609ce90bc0a
helloRahulkp <hello.rahulkp@gmail.com>
2026-03-13 09:51:48 +0530

    packages

---
+++ IBM GenAI Engineering/Developing AI Applications with Python and Flask/mypackage/__init__.py
@@ -0,0 +1,9 @@
+from . import module01
+from . import module02
+
+
+# to test package open terminal
+# change dir to root of package containing
+# open python interpreter on terminal
+# run import package name
+# if no error then working
